aa0cd0ec45d12d177114d26e2d71dc595819e977
younghoon <hoons0131@gmail.com>
2017-08-06 17:19:06 +0900

    todomvc 파일 분리

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+const engine = require('ejs-locals');
+const router = require('./router/index')(app);
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('ejs', engine);
+
+let server = app.use(express.static('views')).listen(port, () => {
+    console.log(`Express server has started on port:${port}`);
+});
